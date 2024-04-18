e974e1d86d3a406910a84c9b969e34362170857d
Zhang690683220 <zhangbo_china@live.cn>
2024-04-17 18:32:42 -0600

    add error code for utils functions

--- include/dspaces-common.h
+++ include/dspaces-common.h
@@ -25,8 +25,7 @@ extern "C" {
 #define dspaces_ERR_UNKNOWN_OBJ -8 /* Could not find the object*/
 #define dspaces_ERR_END -9         /* End of range for valid error codes */
 #define dspaces_ERR_CUDA -10         /* Error related to the CUDA */
-#define dspaces_ERR_GDRCOPY -11         /* Error related to the GDRCopy */
-#define dspaces_ERR_UTILS -12
+#define dspaces_ERR_UTILS -11
 
 #define DS_OBJ_RESIZE 0x02
 
